d50e6cb8a184d18c80092c2d62e528c3b8539446
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2017-08-25 17:55:30 +0300

    sunscribe to return stream future

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate poloniex;
+extern crate tokio_core;
+extern crate websocket;
+
+use poloniex::push;
+use tokio_core::reactor::Core;
+use websocket::WebSocketError::{ResponseError};
+
 fn main() {
-  
+  let mut core = Core::new().unwrap();
+  let client = push::connect(&core.handle());
+  core.run(client).unwrap();
 }
