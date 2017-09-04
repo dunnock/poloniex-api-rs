c7e6d65d942a928c1d58d07be9e433388cd8e1ca
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2017-09-04 16:42:20 +0300

    fix logger refactoring

--- src/actors/logger.rs
+++ src/actors/logger.rs
@@ -1,6 +1,8 @@
 use time::get_time;
 use super::Actor;
+use ::error::PoloError;
 
+#[derive(Clone)]
 pub struct Logger;
 
 impl Logger {
@@ -10,8 +12,9 @@ impl Logger {
 }
 
 impl Actor for Logger {
-  fn process_message(&mut self, msg: String) -> Result<(),_> {
+  fn process_message(&mut self, msg: String) -> Result<(),PoloError> {
     let ts = get_time();
     println!("{}.{} {}", ts.sec, ts.nsec/1000000, msg);
+    Ok(())
   }
 }
