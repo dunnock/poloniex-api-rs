aac502b497f4ad704acad429f9b0a6d585c1edde
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2017-09-04 16:38:14 +0300

    refactor logger to actor

--- src/actors/logger.rs
+++ src/actors/logger.rs
@@ -1,15 +1,17 @@
-use std::thread::{self, JoinHandle};
 use time::get_time;
-use bus::BusReader;
+use super::Actor;
 
-// TODO: convert to struct with trait
-pub fn start(mut channel: BusReader<String>) -> JoinHandle<Result<(),String>> {
-  thread::spawn(move || 
-    loop {
-      // TODO: maybe send reference, not String?
-      let msg: String = channel.recv().map_err(|err| err.to_string())?;
-      let ts = get_time();
-      println!("{}.{} {}", ts.sec, ts.nsec/1000000, msg);
-    }
-  )
+pub struct Logger;
+
+impl Logger {
+  pub fn new() -> Logger {
+    Logger { }
+  }
+}
+
+impl Actor for Logger {
+  fn process_message(&mut self, msg: String) -> Result<(),_> {
+    let ts = get_time();
+    println!("{}.{} {}", ts.sec, ts.nsec/1000000, msg);
+  }
 }
