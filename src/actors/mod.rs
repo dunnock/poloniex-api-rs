c6e3f44a47a1b62787eb92230ed6b50509648ec6
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2019-12-13 22:46:02 +0200

    add wasi support when compiling without ws feature

--- src/actors/mod.rs
+++ src/actors/mod.rs
@@ -1,10 +1,26 @@
 pub mod book;
+
+pub use book::Accountant;
+
+#[cfg(not(target_arch="wasm32"))]
 pub mod logger;
+#[cfg(not(target_arch="wasm32"))]
+pub use logger::Logger;
+
+#[cfg(not(target_arch="wasm32"))]
+pub(crate) use time::get_time;
+#[cfg(target_os="wasi")]
+pub fn get_time() -> time::Timespec {
+    let ts = unsafe {
+        wasi::clock_time_get(
+            wasi::CLOCKID_MONOTONIC, 1, // precision... seems ignored though?
+        )
+        .unwrap()
+    };
+    time::Timespec::new((ts / 1_000_000_000) as i64, (ts % 1_000_000_000) as i32)
+}
 
 use crate::error::PoloError;
-
-pub type Accountant = book::Accountant;
-pub type Logger = logger::Logger;
 
 pub trait Processor {
     fn process_message(&mut self, msg: String) -> Result<(), PoloError>;
